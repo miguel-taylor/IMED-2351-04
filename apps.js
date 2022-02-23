ed44c5a5ae44eb10a192c9d66bac9765ab3539d3
Miguel Taylor <migueltaylor.tx@gmail.com>
2022-02-23 13:08:34 -0600

    new

--- apps.js
+++ apps.js
@@ -2,7 +2,7 @@ let dogWeight = 40;
 let dogType = "corgi";
 const x = 50;
 
-document.getElementById("question").innerHTML= "Hello I have a ${dogType} that weights ${dogWeight}, Can I bring my ${dogType} to the apartment?";
+document.getElementById("question").innerHTML= `Hello I have a ${dogType} that weights ${dogWeight}, Can I bring my ${dogType} to the apartment?`;
 if (x > 50){
     document.write("Your dog is not allowed in our apartment");
 } else { document.write(" Your dog is allowed in our apartment");
