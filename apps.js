d775a383260cb333668c97bf399610b64a6b0366
Miguel Taylor <migueltaylor.tx@gmail.com>
2022-03-10 15:37:07 -0600

    update

--- apps.js
+++ apps.js
@@ -7,3 +7,16 @@ if (x > 50){
     document.write("Your dog is not allowed in our apartment");
 } else { document.write(" Your dog is allowed in our apartment");
 }
+switch ( dogWeight ) {
+    case 10:
+        document.write("<p> Your pet fee will be 100 </p>");
+        break;
+    case 20:
+        document.write("<p> Your pet fee will be 150</p>");
+        break;
+    case 40:
+        document.write("Your pet fee will be 200");
+        break;
+    default: 
+        document.write("Standard fee is 100");
+}
